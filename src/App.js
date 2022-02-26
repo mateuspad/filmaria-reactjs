537bcb7e38a4a9e52b2c0bfbda9e5f3a0b619c51
Mateus Padilha <mateuspmota2010@gmail.com>
2022-02-26 14:40:57 -0300

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Routes from "./routes";
 import './styles.css';
-//https://sujeitoprogramador.com/r-api/?api=filmes/
+
 export default function App(){
   return(
     <div className="app">
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ export default function Header(){
     return(
         <Header>
             <Link className='logo' to="/">Filmaria</Link>
-            <Link className='favoritos' to="/">Salvos</Link>
+            <Link className='favoritos' to="/favoritos">Salvos</Link>
         </Header>
     )
 }
